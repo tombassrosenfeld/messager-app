40b2fe9adaa3ac7be18e70d952b249f2d241508a
Tom Rosenfeld <thomasrosenfeld@hotmail.com>
2021-10-16 21:57:30 +0100

    updated reducer with user and chat actions

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,6 +1,18 @@
 
+const updateChats = (state, { chats, messages }) => {
+    return { 
+        ...state,
+        chats,
+        messages,
+    }
+}
+
+
 const reducer = (state, action) => {
     switch (action.type) {
+        case "UPDATE_USERS": return {...state, users: action.users };
+        
+        case "UPDATE_CHATS": return updateChats(state, action);
 
         default: return state;
     }
