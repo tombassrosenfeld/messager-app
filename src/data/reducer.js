c76b5d21f738db366ef26858849e8d07159a058e
Tom Rosenfeld <thomasrosenfeld@hotmail.com>
2021-10-18 23:13:14 +0100

    added add message action type to reducer

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -7,13 +7,23 @@ const updateChats = (state, { chats, messages }) => {
     }
 }
 
+const addMessage = (state, { message }) => {
+    return {
+        ...state, 
+        messages: [
+            ...state.messages,
+            message,
+        ]
+    }
+}
 
 const reducer = (state, action) => {
     switch (action.type) {
         case "SET_USER": return { ...state, user: action.user }
+        case "SET_CURRENT_CHAT": return { ...state, currentChat: action.chatID }
         case "UPDATE_USERS": return { ...state, users: action.users };
-        
         case "UPDATE_CHATS": return updateChats(state, action);
+        case "ADD_MESSAGE": return addMessage(state, action)
 
         default: return state;
     }
