8835327fa3edbc2218764eb8733efe0971819912
Tom Rosenfeld <thomasrosenfeld@hotmail.com>
2021-10-18 23:09:48 +0100

    updated and styled chats

--- src/components/Chats/index.js
+++ src/components/Chats/index.js
@@ -1,24 +1,30 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import axios from '../../data/axios'
+
+import './Chats.css'
 
 const Chats = () => {
     const dispatch = useDispatch();
-    const userID = useSelector((state) => state.user.id);
-    useEffect(() => {
-        axios.get(`users/${ userID }/chats`).then((data) => {
-            const { chats, messages } = data.data;
-            dispatch({
-                type: "UPDATE_CHATS",
-                chats,
-                messages,
-            })
+    const chats = useSelector((state) => state.chats);
+   
+    const handleClick = (chatID) => {
+        dispatch({
+            type: "SET_CURRENT_CHAT",
+            chatID
         })
-    })
+    }
+
     return (
-        <>
+        <div className="chats">
             <h2>Chats</h2>
-        </>
+            <div className="chat-list">
+                {
+                    chats.map(chat => (
+                        <button className="chat__button" key={chat.id} onClick={() => handleClick(chat.id) }>{chat.name}</button>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
 
